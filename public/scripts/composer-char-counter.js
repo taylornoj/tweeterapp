6dc28c7faa83992351cf2e1ac317e30ebcde4573
Taylor Noj <taylornoj@gmail.com>
2021-11-16 21:36:52 +0000

    Get value in textarea - am able to console.log the number of characters left in DevTools

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,7 @@
 $(document).ready(() => {
-
-}
+  $("#tweet-text").on("input", function () {
+    const characterLimit = 140;
+    const charactersRemain = characterLimit - $(this).val().length;
+    console.log(charactersRemain);
+  })
+});
